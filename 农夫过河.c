ce9db8c5d30ae760be4a8edc14ae271f7225aef7
Steven Wang <32712425+catmia0w@users.noreply.github.com>
2017-11-14 09:14:28 +0800

    Update 农夫过河.c

--- 农夫过河.c
+++ 农夫过河.c
@@ -1,3 +1,16 @@
 #define MaxVertexNum 10
-typeddf enum{FALSE,TRUE}Boolean;
+typedef enum{FALSE,TRUE}Boolean;
 Boolean visited[MaxVertexNum];
+int path[MaxVertexNum];
+int locate(MGraph *G,int F,int W,int S,int V)
+{
+  int i;
+  for(i=0;i<G->vertexNum;i++)
+    if(G->vertexs[i].farmer==F&&G->vertexs[i].wolf==W&&G->vertexs[i].sheep==S&&G->vertexs[i].vegetable==V)
+      return(i);
+  return(-1);
+}
+int is_safe(int F,int W,int S,int V)
+{
+  
+}
