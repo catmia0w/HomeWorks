729943fa805eb0866fb842ff8316ec68e677362f
catMiaow <1175664820@qq.com>
2019-06-17 16:19:39 +0800

    nothing

--- Linux_Operating_System_and_Programming_Linux/demo3/3-2.c
+++ Linux_Operating_System_and_Programming_Linux/demo3/3-2.c
@@ -5,17 +5,18 @@
 
 int main()
 {
-//    char block[1024]; 
+    
     char c; 
     int in, out; 
     int nread;
 
     in = open("file.in", O_RDONLY); 
-out = open("file.out", O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
+    out = open("file.out", O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
 //将注释打开，并将两条语句的后一句注释掉，重新编译执行。
-//    while((nread = read(in,block,sizeof(block))) > 0)
-    while((nread = read(in,&c,sizeof(c))) > 0)
-//        write(out,block,nread);
-        write(out,&c,nread);
+    char block[1024]; 
+    while((nread = read(in,block,sizeof(block))) > 0)
+        write(out,block,nread);
+//    while((nread = read(in,&c,sizeof(c))) > 0)
+  //      write(out,&c,nread);
 
 }
